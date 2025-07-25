6884a59897c5ecdda6a66a19e4d75dee978af546
Ian Kirk <iankirk24@gmail.com>
2025-07-24 21:41:49 -0400

    Added more js

--- travel_recommendation.js
+++ travel_recommendation.js
@@ -1,25 +1,37 @@
 const searchBtn = document.getElementById('searchBtn');
 const input = document.getElementById('input').value;
 const clearBtn = document.getElementById('clearBtn');
-    const apiUrl = './travel_recommendation_api.json';
+const recContainer = document.getElementById('recContainer');
+const apiUrl = './travel_recommendation_api.json';
 
 
 function getApi () {
 
     fetch(apiUrl).then((response) => response.json())
                 .then((data) => {
-            const recContainer = document.getElementById('recContainer');
-            if (input.toLowerCase() === 'beaches' || 'beach'){
-            const dataHTML = data.map((places)=> {
-            `<img src='${places.beaches.img}>
-             <h3>${places.beaches.name}</h3>
-             <p>${places.beaches.description}</p>
-             <button>Visit</button>`
-             recContainer.innerHTML = dataHTML;
-        })
-                }})
+                    console.log(data);
+                    const mapData = data.beaches.map(item => 
+                        `<div class="container">
+                        <img src='${item.imageUrl}'>
+                        <h3>${item.name}</h3>
+                        <p>${item.description}</p>
+                        <button>Visit</button>
+                        </div><br> `)
+                    recContainer.innerHTML = mapData.join('');
+            })
 }
 
 searchBtn.addEventListener('click', getApi)
     
-// getApi();
+getApi();
+
+
+        //     if (input.toLowerCase() === 'beaches' || 'beach'){
+        //     const dataHTML = data.map((places)=> {
+        //     `<img src='${places.beaches.img}>
+        //      <h3>${places.beaches.name}</h3>
+        //      <p>${places.beaches.description}</p>
+        //      <button>Visit</button>`
+        //      recContainer.innerHTML = dataHTML;
+        // })
+        //         }
