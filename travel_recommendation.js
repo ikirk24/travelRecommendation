a31469bb29fd7a603ffbbfae35686077962437fc
Ian Kirk <iankirk24@gmail.com>
2025-07-25 02:12:46 -0400

    Almost done

--- travel_recommendation.js
+++ travel_recommendation.js
@@ -1,29 +1,48 @@
 const searchBtn = document.getElementById('searchBtn');
-const input = document.getElementById('input').value;
+const input = document.getElementById('input');
 const clearBtn = document.getElementById('clearBtn');
 const recContainer = document.getElementById('recContainer');
 const apiUrl = './travel_recommendation_api.json';
 
 
 function getApi () {
-
     fetch(apiUrl).then((response) => response.json())
                 .then((data) => {
-                    console.log(data);
-                    const mapData = data.beaches.map(item => 
+                    let inputWord = input.value.toLowerCase();
+                    
+                    if (inputWord === 'beaches' || 'beach' ){
+                    const beachData = data.beaches.map(item => 
                         `<div class="container">
                         <img src='${item.imageUrl}'>
                         <h3>${item.name}</h3>
                         <p>${item.description}</p>
                         <button>Visit</button>
                         </div><br> `)
-                    recContainer.innerHTML = mapData.join('');
-            })
+                    recContainer.innerHTML = beachData.join('');
+                    console.log(inputWord)
+
+                 } else if (inputWord === 'temples' || 'temple'){
+                    
+                    const templeData = data.temples.map(item => 
+                        `<div class="container">
+                        <img src='${item.imageUrl}'>
+                        <h3>${item.name}</h3>
+                        <p>${item.description}</p>
+                        <button>Visit</button>
+                        </div><br> `)
+                        recContainer.innerHTML = templeData.join('');
+                        return;
+                 }
+                
+ })
 }
 
+
+function inputTest() {
+    console.log(input.value);
+}
 searchBtn.addEventListener('click', getApi)
-    
-getApi();
+// getApi();
 
 
         //     if (input.toLowerCase() === 'beaches' || 'beach'){
