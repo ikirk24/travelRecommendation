8106c09b290e6b313212dc48139cc52980b4e03b
Ian Kirk <iankirk24@gmail.com>
2025-07-27 04:33:15 -0400

    Finished project

--- travel_recommendation.js
+++ travel_recommendation.js
@@ -1,28 +1,26 @@
 const searchBtn = document.getElementById('searchBtn');
-const input = document.getElementById('input');
 const clearBtn = document.getElementById('clearBtn');
-const recContainer = document.getElementById('recContainer');
 const apiUrl = './travel_recommendation_api.json';
+const recContainer = document.getElementById('recContainer');
+const inputSpot = document.getElementById('input');
+    
 
 
 function getApi () {
     fetch(apiUrl).then((response) => response.json())
                 .then((data) => {
-                    let inputWord = input.value.toLowerCase();
-                    
-                    if (inputWord === 'beaches' || 'beach' ){
+
+              const input = document.getElementById('input').value;
+
                     const beachData = data.beaches.map(item => 
                         `<div class="container">
                         <img src='${item.imageUrl}'>
                         <h3>${item.name}</h3>
                         <p>${item.description}</p>
                         <button>Visit</button>
-                        </div><br> `)
-                    recContainer.innerHTML = beachData.join('');
-                    console.log(inputWord)
+                        </div><br> `);
 
-                 } else if (inputWord === 'temples' || 'temple'){
-                    
+
                     const templeData = data.temples.map(item => 
                         `<div class="container">
                         <img src='${item.imageUrl}'>
@@ -30,27 +28,50 @@ function getApi () {
                         <p>${item.description}</p>
                         <button>Visit</button>
                         </div><br> `)
-                        recContainer.innerHTML = templeData.join('');
-                        return;
-                 }
-                
- })
+
+                    const countryData = data.countries.map(item => 
+                        item.cities.map(country => 
+                            
+                            `<div class="container">
+                            <img src='${country.imageUrl}'>
+                            <h3>${country.name}</h3>
+                            <p>${country.description}</p>
+                            <button>Visit</button>
+                                </div><br> `))
+
+
+                        
+
+                    if (input.toLowerCase() === 'beach' || input.toLowerCase() === 'beaches') {
+                        recContainer.innerHTML = beachData;
+                    } else if (input.toLowerCase() === 'temple' || input.toLowerCase() === 'temples'){
+                        recContainer.innerHTML = templeData; 
+                    } else if (input.toLowerCase() === 'country' || input.toLowerCase() === 'countries'){
+                        recContainer.innerHTML = countryData;
+                    } else {
+                        alert("Search 'Beaches', 'Temples', or 'Countries'");
+                    }
+                     document.getElementById('input').value = '';
+
+                    
+                            
+            }
+            )
+
 }
 
+function clear () {
+    recContainer.innerHTML = '';
+    document.getElementById('input').value = '';
 
-function inputTest() {
-    console.log(input.value);
+
 }
-searchBtn.addEventListener('click', getApi)
-// getApi();
 
+clearBtn.addEventListener('click', clear);
+searchBtn.addEventListener('click', getApi);
+inputSpot.addEventListener('keydown', function(e) {
+    if (e.key === 'Enter') {
+        getApi();
+    }
 
-        //     if (input.toLowerCase() === 'beaches' || 'beach'){
-        //     const dataHTML = data.map((places)=> {
-        //     `<img src='${places.beaches.img}>
-        //      <h3>${places.beaches.name}</h3>
-        //      <p>${places.beaches.description}</p>
-        //      <button>Visit</button>`
-        //      recContainer.innerHTML = dataHTML;
-        // })
-        //         }
+} )
